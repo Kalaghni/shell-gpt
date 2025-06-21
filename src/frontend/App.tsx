452bcc49bde47069e5772b64658c69e451a4df98
Josh Wood <76712541+Kalaghni@users.noreply.github.com>
2025-06-21 15:55:36 -0400

    stuff

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -4,23 +4,26 @@ import {SidebarInset, SidebarProvider} from "@/components/ui/sidebar.tsx";
 import AppHeader from "@/components/app-header.tsx";
 import Shell from "@/frontend/pages/Shell.tsx";
 import {ThemeProvider} from "@/components/theme-provider.tsx";
-import {Route, Routes} from "react-router-dom";
+import {HashRouter, Route, Routes} from "react-router-dom";
 import Settings from "@/frontend/pages/Settings.tsx";
 
 function App() {
+
     return (
-        <ThemeProvider>
-            <SidebarProvider defaultOpen={false}>
-                <AppSidebar/>
-                <SidebarInset>
-                    <AppHeader/>
-                    <Routes>
-                        <Route path="/" element={<Shell/>} />
-                        <Route path="/settings" element={<Settings/> }/>
-                    </Routes>
-                </SidebarInset>
-            </SidebarProvider>
-        </ThemeProvider>
+        <HashRouter>
+            <ThemeProvider>
+                <SidebarProvider defaultOpen={false}>
+                    <AppSidebar/>
+                    <SidebarInset>
+                        <AppHeader/>
+                        <Routes>
+                            <Route path="/" element={<Shell/>} />
+                            <Route path="/settings" element={<Settings/> }/>
+                        </Routes>
+                    </SidebarInset>
+                </SidebarProvider>
+            </ThemeProvider>
+        </HashRouter>
     )
 }
 
--- src/frontend/main.tsx
+++ src/frontend/main.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import {BrowserRouter} from "react-router-dom";
+import {StrictMode} from "react";
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+    <StrictMode>
+        <App />
+    </StrictMode>,
 )
